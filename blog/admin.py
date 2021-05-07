adece14161c4ad39777ac2cde319bd5a43e7a037
AkmatovEldi <akmatoveldi@gmail.com>
2021-05-07 13:55:02 +0600

    create templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        """
+        Переопределяем метод сохранения модели
+        """
+
+        if not change:  # Проверяем что запись только создаётся
+            obj.author = request.user  # Присваеваем полю автор текущего пользователя
+
+        super(PostAdmin, self).save_model(
+            request=request,
+            obj=obj,
+            form=form,
+            change=change
+        )
